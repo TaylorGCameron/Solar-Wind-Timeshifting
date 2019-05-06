b2518ec6d0dd5123587efce67cf46df9781ef396
TaylorGCameron <46903242+TaylorGCameron@users.noreply.github.com>
2019-05-06 10:49:17 -0600

    Removed redundant function angle_between_signed

--- useful_functions.py
+++ useful_functions.py
@@ -116,15 +116,6 @@ def angle_between(v1, v2):
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
     
-def angle_between_signed(v1, v2, vn):
-    """ Returns the angle in radians between vectors 'v1' and 'v2'
-    """
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-
-    sign = np.dot(vn, np.cross(v1,v2))
-    
-    return sign*np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
     
 def fill_nan(A):
     '''
