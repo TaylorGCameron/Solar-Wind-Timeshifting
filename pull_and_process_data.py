72651ea5f97bb7361e29d6a42e67ed728301d8cd
TaylorGCameron <46903242+TaylorGCameron@users.noreply.github.com>
2019-05-08 11:08:16 -0600

    Updated pull_and_process_data.py to reflect new package structure

--- pull_and_process_data.py
+++ pull_and_process_data.py
@@ -6,19 +6,18 @@ Created on Tue May  7 18:07:35 2019
 """
 
 #import timeshifting as ts
-import timeshifting.pull_data as pull
-import timeshifting.process_data as process
+import timeshifting.data as data
 
 #Pull Data from CDAWeb
 for i in range(2000, 2010):
-    pull.pull_ACE(i)
-    pull.pull_ACE_B(i)
-    pull.pull_GOES(i)
+    data.pull_ACE(i)
+    data.pull_ACE_B(i)
+    data.pull_GOES(i)
     
 for i in range(2000, 2010):
     #Calculates time indices for 2 hour long intervals each separated by half an hour
-    process.calc_time_indices(i)
+    data.calc_time_indices(i)
     
 for i in range(2000, 2010):
     #Calculates time indices for 2 hour long intervals each separated by half an hour
-    process.generate_ideal_timeshifts(i)
+    data.generate_ideal_timeshifts(i)
