46cd42a89cd360739a4989f894d9b75a691faaf3
TaylorGCameron <46903242+TaylorGCameron@users.noreply.github.com>
2019-05-08 14:41:00 -0600

    Modified data processing functions to work on a range of years specified in config.par

--- pull_and_process_data.py
+++ pull_and_process_data.py
@@ -4,20 +4,13 @@ Created on Tue May  7 18:07:35 2019
 
 @author: Taylor
 """
-
-#import timeshifting as ts
 import timeshifting.data as data
 
 #Pull Data from CDAWeb
-for i in range(2000, 2010):
-    data.pull_ACE(i)
-    data.pull_ACE_B(i)
-    data.pull_GOES(i)
+data.pull_data()
     
-for i in range(2000, 2010):
-    #Calculates time indices for 2 hour long intervals each separated by half an hour
-    data.calc_time_indices(i)
+#Calculates time indices for 2 hour long intervals each separated by half an hour
+data.calc_time_indices()
     
-for i in range(2000, 2010):
-    #Calculates time indices for 2 hour long intervals each separated by half an hour
-    data.generate_ideal_timeshifts(i)
+#Calculates time indices for 2 hour long intervals each separated by half an hour
+data.generate_ideal_timeshifts()
