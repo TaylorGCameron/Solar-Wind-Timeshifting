484247d2bf21bb340c36c9201cddf7df1271a94d
TaylorGCameron <46903242+TaylorGCameron@users.noreply.github.com>
2019-05-21 12:14:43 -0600

    Updated demonstration to reflect changes in package

--- demonstration.py
+++ demonstration.py
@@ -13,39 +13,39 @@ start = time.time()
 
 #Timeshifting stuff
 
-o = False
-
-ts.calc_timeshifts('flat', 'flat', overwrite = o)
-
-ts.calc_timeshifts('MVAB0', 'MVAB0r5', ratio = 5, overwrite = o)
-ts.calc_timeshifts('MVAB0', 'MVAB0r2', ratio = 2, overwrite = o)
-
-ts.calc_timeshifts('MVAB', 'MVABr2', ratio = 2, overwrite = o)
-ts.calc_timeshifts('MVAB', 'MVABr5', ratio = 5, overwrite = o)
-
-ts.calc_timeshifts('front_angle', 'angle_15', angle = 15., overwrite = o)
-
-ts.calc_timeshifts('jackel', 'jackel', overwrite = o)
-
-ts.calc_timeshifts('empirical', 'empirical', overwrite = o)
-
-ts.calc_timeshifts('cross_product', 'cross_product', overwrite = o)
-
-ts.evaluate_method('MVAB0r5', 0.3)
-ts.evaluate_method('MVAB0r2', 0.3)
-
-ts.evaluate_method('MVABr5', 0.3)
-ts.evaluate_method('MVABr2', 0.3)
-
-ts.evaluate_method('flat', 0.3)
-
-ts.evaluate_method('angle_15', 0.3)
-
-ts.evaluate_method('empirical', 0.3)
-
-ts.evaluate_method('cross_product', 0.3)
-
-ts.evaluate_method('jackel', 0.3)
+#o = False
+#
+#ts.calc_timeshifts('flat', 'flat', overwrite = o)
+#
+#ts.calc_timeshifts('MVAB0', 'MVAB0r5', ratio = 5, overwrite = o)
+#ts.calc_timeshifts('MVAB0', 'MVAB0r2', ratio = 2, overwrite = o)
+#
+#ts.calc_timeshifts('MVAB', 'MVABr2', ratio = 2, overwrite = o)
+#ts.calc_timeshifts('MVAB', 'MVABr5', ratio = 5, overwrite = o)
+#
+#ts.calc_timeshifts('front_angle', 'angle_15', angle = 15., overwrite = o)
+#
+#ts.calc_timeshifts('jackel', 'jackel', overwrite = o)
+#
+#ts.calc_timeshifts('empirical', 'empirical', overwrite = o)
+#
+#ts.calc_timeshifts('cross_product', 'cross_product', overwrite = o)
+#
+#ts.evaluate_method('MVAB0r5', 0.3)
+#ts.evaluate_method('MVAB0r2', 0.3)
+#
+#ts.evaluate_method('MVABr5', 0.3)
+#ts.evaluate_method('MVABr2', 0.3)
+#
+#ts.evaluate_method('flat', 0.3)
+#
+#ts.evaluate_method('angle_15', 0.3)
+#
+#ts.evaluate_method('empirical', 0.3)
+#
+#ts.evaluate_method('cross_product', 0.3)
+#
+#ts.evaluate_method('jackel', 0.3)
 
 #Neural Network stuff
 
@@ -65,23 +65,23 @@ def input_output(self, inds):
     Y_data = self.ideal_shifts[inds]/60.
     return X_data, Y_data
 '''
-#a = nn.Network(
-#                 custom_func = f, 
-#                 filename = 'test2.npy',
-#                 corr_min = 0.7,
-#                 n_train = 2500,
-#                 min_shift = 10.,
-#                 layout = np.array([10,10,10]),
-#                 n_models = 10,
-#                 optimizer = 'adam',
-#                 loss = 'mae',
-#                 metrics = ['mse'],
-#                 n_epochs = 1000,
-#                 batch_size = 50,
-#               )
-#
-#a.loadData()
-#a.run()
+a = nn.Network(
+                 custom_func = f, 
+                 filename = 'test2.npy',
+                 training_corr_min = 0.7,
+                 n_train = 2500,
+                 min_shift = 2.,
+                 layout = np.array([10,10,10]),
+                 n_models = 10,
+                 optimizer = 'adam',
+                 loss = 'mae',
+                 metrics = ['mse'],
+                 n_epochs = 100,
+                 batch_size = 50,
+               )
+
+a.loadData()
+a.run()
 
 b = nn.loadNetwork('test2.npy')
 b.printSummary()
